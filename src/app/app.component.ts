51a592d661e00b368bba8018acddace13ccd24b3
Quy Tran <quytran288@gmail.com>
2017-06-27 00:18:16 -0500

    Using Subject for transfer data cross Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "./user.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  user1Activated: boolean = false;
+  user2Activated: boolean = false;
+
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.activatedUser.subscribe(
+      (id: number) => {
+        this.user1Activated = id === 1 ? true : false;
+        this.user2Activated = id === 2 ? true : false;
+        // setInterval(
+        //   () => { console.log(id); }, 1000);
+      }
+    );
+  }
 }
