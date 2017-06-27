aa1fea6ac3e06d33bcdb7fc5d0858335491ce199
Quy Tran <quytran288@gmail.com>
2017-06-26 23:00:16 -0500

    Create a sample Obserable from scratch

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from "rxjs/Observable";
 import "rxjs/Rx";
+import {Observer} from "rxjs/Observer";
 
 @Component({
   selector: 'app-home',
@@ -12,11 +13,43 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = Observable.interval(1000);
+    // const myNumbers = Observable.interval(1000);
+    //
+    // myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   }
+    // );
 
-    myNumbers.subscribe(
-      (number: number) => {
-        console.log(number);
+    const myObservable = Observable.create(
+      (observer: Observer<string>) => {
+        setTimeout(() => {
+          observer.next("first data package");
+        }, 2000);
+        setTimeout(() => {
+          observer.next("second data package");
+        }, 4000);
+        setTimeout(() => {
+          // observer.error("this does not work!")
+          observer.complete();
+        }, 5000);
+        setTimeout(() => {
+          observer.next("first data package again");
+        }, 2000);
+        setTimeout(() => {
+          observer.next("third data package");
+        }, 6000);
+      });
+
+    myObservable.subscribe(
+      (data: string) => {
+        console.log(data);
+      },
+      (error: string) => {
+        console.log(error);
+      },
+      () => {
+        console.log("completed");
       }
     );
   }
